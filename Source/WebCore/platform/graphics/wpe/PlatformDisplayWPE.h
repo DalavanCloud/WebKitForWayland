c9bb78b50a99b5f2c64a7521aa82d0a621542496
Zan Dobersek <zdobersek@igalia.com>
2016-02-10 01:55:36 -0800

    PlatformDisplayWPE: support initialization with authentication data.

--- Source/WebCore/platform/graphics/wpe/PlatformDisplayWPE.h
+++ Source/WebCore/platform/graphics/wpe/PlatformDisplayWPE.h
@@ -43,6 +43,8 @@ class IntSize;
 
 class PlatformDisplayWPE final : public PlatformDisplay {
 public:
+    static void initialize(std::pair<const uint8_t*, size_t>);
+
     PlatformDisplayWPE();
     virtual ~PlatformDisplayWPE();
 
